642624f84c95c2107619e51707c4d65b5c036a71
marziye <marziyeslmn313@gmail.com>
2022-08-31 00:32:23 -0700

    start project

---
+++ src/Article.java
@@ -0,0 +1,75 @@
+public class Article {
+    private int id;
+    private String title;
+    private String  brief;
+    private String  content;
+    private String createDate;
+    private String  isPublished;
+    private User user_id;
+
+    public Article(int id, String title, String brief, String content, String createDate, String isPublished, User user_id) {
+        this.id = id;
+        this.title = title;
+        this.brief = brief;
+        this.content = content;
+        this.createDate = createDate;
+        this.isPublished = isPublished;
+        this.user_id = user_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBrief() {
+        return brief;
+    }
+
+    public void setBrief(String brief) {
+        this.brief = brief;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getIsPublished() {
+        return isPublished;
+    }
+
+    public void setIsPublished(String isPublished) {
+        this.isPublished = isPublished;
+    }
+
+    public User getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(User user_id) {
+        this.user_id = user_id;
+    }
+}
